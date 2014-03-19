d4ba3d172cdb99dd2f17a3fcd62c963a3eb527f3
Eddy Hernandez <eddy.hernandez@rackspace.com>
2014-03-18 21:42:19 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Passport-Keystone
+
+OpenStack Keystone authentication strategy for [Passport](http://passportjs.org/) and Node.js
+
+## Install
+
+    $ npm install passport-keystone
+
+## Usage
+
+#### Configure Strategy
+
+The keystone authentication strategy authenticates users using a username and
+password.  The strategy requires a `verify` callback, which accepts these
+credentials and calls `done` providing a user.
+
+    passport.use(new KeystoneStrategy({
+       region: your.region,
+       url: your.authUrl,
+       usernameField: 'username',
+       passwordField: 'password'
+      },
+      function(user, done) {
+        return done(null, user);
+      }
+    ));
+
+#### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the `'keystone'` strategy, to
+authenticate requests.
+
+For example, as route middleware in an [Express](http://expressjs.com/)
+application:
+
+    app.post('/login',
+      passport.authenticate('keystone', { failureRedirect: '/login' }),
+      function(req, res) {
+        res.redirect('/');
+      });
---
+++ examples/app.js
@@ -0,0 +1,136 @@
+var express = require('express'),
+passport = require('passport'),
+flash = require('connect-flash'),
+KeystoneStrategy = require('../lib/passport-keystone').Strategy;
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the KeystoneStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new KeystoneStrategy(
+  {
+    authUrl: 'https://identity.api.rackspacecloud.com',
+    region: 'ord',
+    passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+  },
+  function(req, identity, done) {
+    if (!req.user) {
+      var user = {};
+
+      user.id    = identity.token.tenant.id +'-'+ identity.user.name;
+      user.username  = identity.user.name;
+      user.tenant  = identity.token.tenant.id;
+      user.token = identity.token.id;
+      req.session.cookie.expires = Date.parse(identity.token.expires) - Date.now();
+      return done(null, user);
+    } else {
+      // user already exists and is logged in, we have to link accounts
+      var user            = req.user; // pull the user out of the session
+      user.token = identity.token.id;
+      req.session.cookie.expires = Date.parse(identity.token.expires) - Date.now();
+      return done(null, user);
+    }
+  })
+);
+
+var app = express();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('ejs', require('ejs-locals'));
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'chachachangeme' }));
+  app.use(flash());
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/../../public'));
+});
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
+
+// POST /login
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+//
+//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+app.post('/login',
+  passport.authenticate('keystone', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+// POST /login
+//   This is an alternative implementation that uses a custom callback to
+//   acheive the same functionality.
+/*
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err) }
+    if (!user) {
+      req.flash('error', info.message);
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/users/' + user.username);
+    });
+  })(req, res, next);
+});
+*/
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000, function() {
+  console.log('Express server listening on port 3000');
+});
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
